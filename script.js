fad8c19ef4b393be31b0fa254d14413ad7aba05d
hbostoniii <60755766+hbostoniii@users.noreply.github.com>
2020-07-06 15:51:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.menu-btn');
+const addShowClass = () => document.querySelector('.main-menu').classList.toggle('show');
+
+btn.addEventListener('click', addShowClass);
+
+
